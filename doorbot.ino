cf6f72a460cf36bb7a6c07caa3fa136335f0f8eb
Jim Allanson <jimallanson@Jims-MacBook-Pro-2.local>
2018-09-16 00:56:15 +0100

    Rewrite to use IotWebConf + run on ESP8266 (NodeMCU)

--- doorbot.ino
+++ doorbot.ino
@@ -1,143 +1,205 @@
 /*
- * This assumes an ESP32 connected to a MFRC522 RFID reader over SPI and an OLED screen connected over I2C
+ * This assumes a NodeMCU connected to a MFRC522 RFID reader over SPI and an OLED screen connected over I2C
  */
 
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <MQTT.h>
+#include <IotWebConf.h>
+
 #include <ESP.h>
-#include <esp_int_wdt.h>
-#include <esp_task_wdt.h>
 #include <U8x8lib.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
-#include <Update.h>
+#include <ESP8266mDNS.h> 
+#include <string.h>
+#include <Hash.h>
 
-const char* deviceName = "doorbot";
-const char* ssid = "ssid";     // your network SSID (name of wifi network)
-const char* password = "password"; // your network password
-const char* host = "xxxxxxxxx.execute-api.eu-west-1.amazonaws.com";  // Membership API URL
-const int wifiTimeoutSeconds = 20;
+// -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
+const String deviceName = "door";
+
+// -- Initial password to connect to the Thing, when it creates an own Access Point.
+const char wifiInitialApPassword[] = "********";
+
+#define STRING_LEN 128
+
+// -- Configuration specific key. The value should be modified if config structure was changed.
+#define CONFIG_VERSION "mqt1"
+
+// -- When CONFIG_PIN is pulled to ground on startup, the Thing will use the initial
+//      password to buld an AP. (E.g. in case of lost password)
+#define CONFIG_PIN 2
+
+// -- Status indicator pin.
+//      First it will light up (kept LOW), on Wifi connection it will blink,
+//      when connected to the Wifi it will turn off (kept HIGH).
+#define STATUS_PIN LED_BUILTIN
+
+// -- Callback method declarations.
+void wifiConnected();
+void configSaved();
+boolean formValidator();
+void mqttMessageReceived(String &topic, String &payload);
+
+DNSServer dnsServer;
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
+WiFiClient net;
+MQTTClient mqttClient;
+
+char mqttServerValue[STRING_LEN];
+char mqttUserNameValue[STRING_LEN];
+char mqttUserPasswordValue[STRING_LEN];
+
+IotWebConf iotWebConf(deviceName.c_str(), &dnsServer, &httpServer, wifiInitialApPassword, CONFIG_VERSION);
+IotWebConfParameter mqttServerParam = IotWebConfParameter("MQTT server", "mqttServer", mqttServerValue, STRING_LEN);
+IotWebConfParameter mqttUserNameParam = IotWebConfParameter("MQTT user", "mqttUser", mqttUserNameValue, STRING_LEN);
+IotWebConfParameter mqttUserPasswordParam = IotWebConfParameter("MQTT password", "mqttPass", mqttUserPasswordValue, STRING_LEN, "password");
+
+boolean needMqttConnect = false;
+boolean needReset = false;
+int pinState = HIGH;
+unsigned long lastReport = 0;
+unsigned long lastMqttConnectionAttempt = 0;
+
+const String requestTopic = "teessideHackspace/access/" +  deviceName + "/request";
+const String responseTopic = "teessideHackspace/access/" + deviceName + "/response";
+
+const int mqttTimeoutSeconds = 10;
 
 // the OLED used
-U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 10, /* data=*/ 0, /* reset=*/ U8X8_PIN_NONE);
 
-#define SS_PIN 21
-#define RST_PIN 22
+#define SS_PIN 4
+#define RST_PIN 5
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-
-
-
-WiFiClientSecure client;
-WebServer server(80);
-
 
 void setup()
 {
-  Serial.begin(115200);
-
+  Serial.begin(9600);
+  setupWebConf();
   setupRFID();
   setupScreen();
-  connectWifi();
   setupMDNS();
-  setupServer();
   resetScreen();
 }
 
 void loop()
 {
+  iotWebConf.doLoop();
+  reconnectMqtt();
+
+  handleReset();
+  
   waitForRFID();
-  server.handleClient();
   delay(1);
 }
 
+static void setupWebConf() {
+  iotWebConf.setStatusPin(STATUS_PIN);
+  iotWebConf.setConfigPin(CONFIG_PIN);
+  iotWebConf.addParameter(&mqttServerParam);
+  iotWebConf.addParameter(&mqttUserNameParam);
+  iotWebConf.addParameter(&mqttUserPasswordParam);
+  iotWebConf.setConfigSavedCallback(&configSaved);
+  iotWebConf.setFormValidator(&formValidator);
+  iotWebConf.setWifiConnectionCallback(&wifiConnected);
+  iotWebConf.setupUpdateServer(&httpUpdater);
 
+  // -- Initializing the configuration.
+  boolean validConfig = iotWebConf.init();
+  if (!validConfig)
+  {
+    mqttServerValue[0] != '\0';
+    mqttUserNameValue[0] != '\0';
+    mqttUserPasswordValue[0] != '\0';
+  }
 
+  // -- Set up required URL handlers on the web server.
+  httpServer.on("/", handleRoot);
+  httpServer.on("/config", []{ iotWebConf.handleConfig(); });
+  httpServer.onNotFound([](){ iotWebConf.handleNotFound(); });
 
-const char* loginIndex = 
- "<form name='loginForm'>"
-    "<table width='20%' bgcolor='A09F9F' align='center'>"
-        "<tr>"
-            "<td colspan=2>"
-                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-                "<br>"
-            "</td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<td>Username:</td>"
-        "<td><input type='text' size=25 name='userid'><br></td>"
-        "</tr>"
-        "<br>"
-        "<br>"
-        "<tr>"
-            "<td>Password:</td>"
-            "<td><input type='Password' size=25 name='pwd'><br></td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-        "</tr>"
-    "</table>"
-"</form>"
-"<script>"
-    "function check(form)"
-    "{"
-    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-    "{"
-    "window.open('/serverIndex')"
-    "}"
-    "else"
-    "{"
-    " alert('Error Password or Username')/*displays error message*/"
-    "}"
-    "}"
-"</script>";
- 
-/*
- * Server Index Page
- */
- 
-const char* serverIndex = 
-"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-   "<input type='file' name='update'>"
-        "<input type='submit' value='Update'>"
-    "</form>"
- "<div id='prg'>progress: 0%</div>"
- "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')" 
- "},"
- "error: function (a, b, c) {"
- "}"
- "});"
- "});"
- "</script>";
+  mqttClient.begin(mqttServerValue, net);
+  mqttClient.onMessage(mqttMessageReceived);
+  
+  Serial.println("Ready.");
+}
 
+void wifiConnected()
+{
+  needMqttConnect = true;
+}
+
+void configSaved()
+{
+  Serial.println("Configuration was updated.");
+  needReset = true;
+}
+
+boolean formValidator()
+{
+  Serial.println("Validating form.");
+  boolean valid = true;
+
+  int l = httpServer.arg(mqttServerParam.id).length();
+  if (l < 3) {
+    mqttServerParam.errorMessage = "Please provide at least 3 characters!";
+    valid = false;
+  }
+
+  return valid;
+}
+
+void reconnectMqtt() {
+  if (needMqttConnect) {
+    if (connectMqtt()) {
+      needMqttConnect = false;
+    }
+  }
+}
+
+boolean connectMqtt() {
+  unsigned long now = millis();
+  if ((lastMqttConnectionAttempt + 1000) > now) {
+    // Do not repeat within 1 sec.
+    return false;
+  }
+  Serial.println("Connecting to MQTT server...");
+  if (!connectMqttOptions()) {
+    lastMqttConnectionAttempt = now;
+    return false;
+  }
+  Serial.println("Connected!");
+
+  mqttClient.subscribe(responseTopic);
+  return true;
+}
+
+boolean connectMqttOptions()
+{
+  boolean result;
+  if (mqttUserPasswordValue[0] != '\0') {
+    result = mqttClient.connect(iotWebConf.getThingName(), mqttUserNameValue, mqttUserPasswordValue);
+  }
+  else if (mqttUserNameValue[0] != '\0') {
+    result = mqttClient.connect(iotWebConf.getThingName(), mqttUserNameValue);
+  }
+  else {
+    result = mqttClient.connect(iotWebConf.getThingName());
+  }
+  return result;
+}
+
+static void handleReset() {
+  if (needReset) {
+    Serial.println("Rebooting after 1 second.");
+    iotWebConf.delay(1000);
+    ESP.restart();
+  }
+}
 
 static void setupRFID() {
   SPI.begin();      // Initiate  SPI bus
@@ -151,7 +213,7 @@ static void setupScreen() {
 
 static void setupMDNS() {
   /*use mdns for host name resolution*/
-  if (!MDNS.begin(deviceName)) { //http://esp32.local
+  if (!MDNS.begin(deviceName.c_str())) { //http://esp32.local
     Serial.println("Error setting up MDNS responder!");
     while (1) {
       delay(1000);
@@ -160,67 +222,29 @@ static void setupMDNS() {
   Serial.println("mDNS responder started");
 }
 
-static void setupServer() {
-  server.on("/", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", loginIndex);
-  });
-  server.on("/serverIndex", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", serverIndex);
-  });
-  /*handling uploading firmware file */
-  server.on("/update", HTTP_POST, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-    ESP.restart();
-  }, []() {
-    HTTPUpload& upload = server.upload();
-    if (upload.status == UPLOAD_FILE_START) {
-      Serial.printf("Update: %s\n", upload.filename.c_str());
-      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
-      /* flashing firmware to ESP*/
-      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_END) {
-      if (Update.end(true)) { //true to set the size to the current progress
-        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-      } else {
-        Update.printError(Serial);
-      }
-    }
-  });
-  server.begin();
-}
-
-
-static void connectWifi() {
-  static uint32_t ms = millis();
-  static uint8_t watchdog = 0;
-  
-  Serial.print("Connecting Wifi: ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
+/**
+ * Handle web requests to "/" path.
+ */
+void handleRoot()
+{
+  // -- Let IotWebConf test and handle captive portal requests.
+  if (iotWebConf.handleCaptivePortal())
   {
-    Serial.print(".");
-    delay(500);
-    if((millis() - ms) > 1000 * wifiTimeoutSeconds) {
-      hard_restart();
-    }
+    // -- Captive portal request were already served.
+    return;
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
+  String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
+  s += "<title>IotWebConf 06 MQTT App</title></head><body>MQTT App demo";
+  s += "<ul>";
+  s += "<li>MQTT server: ";
+  s += mqttServerValue;
+  s += "</ul>";
+  s += "Go to <a href='config'>configure page</a> to change values.";
+  s += "</body></html>\n";
+
+  httpServer.send(200, "text/html", s);
 }
+
 
 static void resetScreen() {
   u8x8.clearDisplay();
@@ -231,54 +255,47 @@ static void resetScreen() {
   u8x8.drawString(0, 4, "your keyfob");
 }
 
-static String callApi(String uid) {
-  String title = "";
-  String headers = "";
-  String body = "";
-  bool finishedHeaders = false;
-  bool currentLineIsBlank = true;
-  bool gotResponse = false;
-  if (client.connect(host, 443)) {
-    Serial.println("connected");
+void mqttMessageReceived(String &topic, String &payload) {
+ Serial.print("Message arrived [");
+ Serial.print(topic);
+ Serial.print("] ");
+ //String body = callApi(content);
+  //Serial.println(payload);
+  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(payload);
+  
+  u8x8.clearDisplay();
+  // Test if parsing succeeds.
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    error();
+  } else {
+    const String error = root["message"];
+    if(error != "") {
+      Serial.println(error);
+      u8x8.drawString(0, 0, "Member not found");
+      u8x8.drawString(0, 1, "If you think");
+      u8x8.drawString(0, 2, "this is an error");
+      u8x8.drawString(0, 3, "please contact");
+      u8x8.drawString(0, 4, "the trustees");
+    } else {
+      
+      const String firstName = root["first_name"];
+      const String lastName = root["last_name"];
+      const String fullName = firstName + " " + lastName;
+      char name[64];
+      fullName.toCharArray(name, 64);
 
-    String URL = "/prod/rfid/" + uid + "/" + deviceName;
-
-    Serial.println(URL);
-
-    client.println("GET " + URL + " HTTP/1.1");
-    client.print("Host: ");
-    client.println(host);
-    client.println("User-Agent: arduino/1.0");
-    client.println("");
-
-    while (!client.available()) {
-      delay(100);
-    }
+      Serial.println("Found user!");
+      Serial.println(name);
     
-    while (client.available()) {
-      char c = client.read();
-
-      if (finishedHeaders) {
-        body = body + c;
-      } else {
-        if (currentLineIsBlank && c == '\n') {
-          finishedHeaders = true;
-        } else {
-          headers = headers + c;
-        }
-      }
-
-      if (c == '\n') {
-        currentLineIsBlank = true;
-      } else if (c != '\r') {
-        currentLineIsBlank = false;
-      }
-      gotResponse = true;
-    }
-    if (gotResponse) {
-      return body;
+      u8x8.drawString(0, 0, "Hello");
+      u8x8.drawString(0, 1, name);
     }
   }
+
+  delay(5000);
+  ESP.restart();
 }
 
 static void waitForRFID()
@@ -290,6 +307,7 @@ static void waitForRFID()
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
+  Serial.println("Found RFID");
   mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); 
   //Show UID on serial monitor
   Serial.print("UID tag :");
@@ -299,7 +317,7 @@ static void waitForRFID()
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : ""));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
@@ -311,47 +329,28 @@ static void waitForRFID()
   u8x8.clearDisplay();
   u8x8.drawString(0, 0, "Please wait...");
 
-  String body = callApi(content);
-  Serial.println(body);
-  StaticJsonBuffer<1000> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(body);
+  const String sha = sha1(content);
+  Serial.println(sha);
   
-  u8x8.clearDisplay();
-  // Test if parsing succeeds.
-  if (!root.success()) {
-    Serial.println("parseObject() failed");
+  mqttClient.publish(requestTopic.c_str(), sha.c_str());
+
+  static uint32_t ms = millis();
+  while ((millis() - ms) < 1000 * mqttTimeoutSeconds) {
+    Serial.println("Waiting for MQTT Response");
+    mqttClient.loop();
+  }
+
+  Serial.println("Did not receive MQTT Response");
+  error();
+}
+
+void error() {
+    u8x8.clearDisplay();
     u8x8.drawString(0, 0, "An error ocurred");
     u8x8.drawString(0, 1, "Please try again");
     u8x8.drawString(0, 2, "If this persists");
     u8x8.drawString(0, 3, "please contact");
     u8x8.drawString(0, 4, "the trustees");
-  } else {
-    const String error = root["message"];
-    if(error != "") {
-      Serial.println(error);
-      u8x8.drawString(0, 0, "Member not found");
-      u8x8.drawString(0, 1, "If you think");
-      u8x8.drawString(0, 2, "this is an error");
-      u8x8.drawString(0, 3, "please contact");
-      u8x8.drawString(0, 4, "the trustees");
-    } else {
-      const String firstName = root["first_name"];
-      const String lastName = root["last_name"];
-      const String fullName = firstName + " " + lastName;
-      char name[64];
-      fullName.toCharArray(name, 64);
-    
-      u8x8.drawString(0, 0, "Hello");
-      u8x8.drawString(0, 1, name);
-    }
-  }
-  
-  delay(5000);
-  ESP.restart();
-}
-
-void hard_restart() {
-  esp_task_wdt_init(1,true);
-  esp_task_wdt_add(NULL);
-  while(true);
+    delay(5000);
+    ESP.restart();
 }
